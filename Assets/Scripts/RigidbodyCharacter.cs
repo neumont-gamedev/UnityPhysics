fd6fe61c896b9f8e700bb17b4e410a6a9ad7a3fa
Raymond L Maple <rmaple@neumont.edu>
2023-03-29 14:11:13 -0600

    Initial Commit

---
+++ Assets/Scripts/RigidbodyCharacter.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacter : MonoBehaviour
+{
+	[SerializeField] float speed = 5;
+	[SerializeField] float turnRate = 180;
+	[SerializeField] float jump = 5;
+	[SerializeField] bool isRelative = false;
+
+	Rigidbody rb;
+	Vector3 position;
+	Quaternion rotation;
+
+	Vector3 direction;
+	Vector3 rotate;
+
+	void Start()
+	{
+		position = transform.position;
+		rotation = transform.rotation;
+
+		rb = GetComponent<Rigidbody>();
+	}
+
+	
+	void Update()
+	{
+		// reset
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			transform.position = position;
+			transform.rotation = rotation;
+			rb.velocity = Vector3.zero;
+			rb.angularVelocity = Vector3.zero;
+		}
+
+		// movement
+		direction = Vector3.zero;
+		direction.z = Input.GetAxis("Vertical");
+		direction.x = Input.GetAxis("Horizontal");
+
+
+		// jump
+		if (Input.GetButtonDown("Jump"))
+		{
+			rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (isRelative)
+		{
+			rb.AddRelativeForce(direction * speed, ForceMode.Force);
+			//rb.AddRelativeTorque(rotate * turnRate);
+		}
+		else
+		{
+			rb.AddForce(direction * speed, ForceMode.Force);
+			//rb.AddTorque(rotate * turnRate);
+		}
+	}
+}
