9e3ccbfc14dcc5f9c3f78df0f1ace90fc272bb31
Raymond L Maple <rmaple@neumont.edu>
2023-04-05 08:26:30 -0600

    character jump

--- Assets/Scripts/ControllerCharacter.cs
+++ Assets/Scripts/ControllerCharacter.cs
@@ -8,7 +8,14 @@ public class ControllerCharacter : MonoBehaviour
 	[SerializeField] float speed;
 	[SerializeField] float turnRate;
 	[SerializeField] float jumpHeight;
+	[SerializeField] float doubleJumpHeight;
 	[SerializeField] float hitForce;
+	[SerializeField, Range(1, 5)] float fallRateMultiplier;
+	[SerializeField, Range(1, 5)] float lowJumpRateMultiplier;
+	[Header("Ground")]
+	[SerializeField] Transform groundTransform;
+	[SerializeField] LayerMask groundLayerMask;
+
 
 	CharacterController characterController;
 	Vector3 velocity = Vector3.zero;
@@ -20,32 +27,60 @@ public class ControllerCharacter : MonoBehaviour
 
 	void Update()
 	{
+		// check if the character is on the ground
+		bool onGround = Physics.CheckSphere(groundTransform.position, 0.2f, groundLayerMask, QueryTriggerInteraction.Ignore);
+
 		// get direction input
 		Vector3 direction = Vector3.zero;
 		direction.x = Input.GetAxis("Horizontal");
 		direction.z = Input.GetAxis("Vertical");
 
+		velocity.x = direction.x * speed;
+		velocity.z = direction.z * speed;
+
 		// set velocity
-		if (characterController.isGrounded)
+		if (onGround)
 		{
-			velocity.x = direction.x * speed;
-			velocity.z = direction.z * speed;
-			
 			if (velocity.y < 0) velocity.y = 0;
 			if (Input.GetButtonDown("Jump"))
 			{
 				velocity.y += Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y);
+				StartCoroutine(DoubleJump());
 			}
 		}
-		velocity.y += Physics.gravity.y * Time.deltaTime;
+
+		// adjust gravity for jump
+		float gravityMultiplier = 1;
+		if (!onGround && velocity.y < 0) gravityMultiplier = fallRateMultiplier;
+		if (!onGround && velocity.y > 0 && !Input.GetButton("Jump")) gravityMultiplier = lowJumpRateMultiplier;
+
+		velocity.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
 
 		// move character
 		characterController.Move(velocity * Time.deltaTime);
 
+		// rotate character to face direction of movement (velocity)
 		Vector3 face = new Vector3(velocity.x, 0, velocity.z);
 		if (face.magnitude > 0)
 		{
 			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(face), Time.deltaTime * turnRate);
+		}
+	}
+
+	IEnumerator DoubleJump()
+	{
+		// wait a little after the jump to allow a double jump
+		yield return new WaitForSeconds(0.01f);
+		// allow a double jump while moving up
+		while (velocity.y > 0)
+		{
+			// if "jump" pressed add jump velocity
+			if (Input.GetButtonDown("Jump"))
+			{
+				velocity.y += Mathf.Sqrt(doubleJumpHeight * -2 * Physics.gravity.y);
+				break;
+			}
+			yield return null;
 		}
 	}
 
